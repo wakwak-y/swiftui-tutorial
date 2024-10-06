e157cd7e5f31135ef565ddbc1997179f2ddaf4ed
Yu Wakui <wak121.y@gmail.com>
2024-10-06 21:34:32 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Yu Wakui on 2024/10/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yu Wakui on 2024/10/06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
